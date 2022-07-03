72f847a76d904c43ba60a85854946a990af5e09e
Matthias Kurz <m.kurz@irregular.at>
2022-07-03 10:26:42 +0200

    semicolon for data line

--- server/fetch-latest-data.php
+++ server/fetch-latest-data.php
@@ -26,7 +26,7 @@ if ($result = $conn->query($sql)) {
         // Uncomment to set timezone to + 4 hours (you can change 4 to any number)
         //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time + 4 hours"));
       
-        echo $row_id . ';' . $row_temp . ';' . $row_heart_rate . ';' . $row_reading_time . '';
+        echo ';' . $row_id . ';' . $row_temp . ';' . $row_heart_rate . ';' . $row_reading_time . ';';
     }
     $result->free();
 }
